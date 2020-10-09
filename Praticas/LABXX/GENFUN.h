a84e180481246592e6c05832d0aadcdc5d897a9e
Aldo <aldo.avilaedu.uag.mx>
2020-10-08 21:42:16 -0500

    Agregando definiciones

---
+++ Praticas/LABXX/GENFUN.h
@@ -0,0 +1,13 @@
+//DataTypes
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+//Definitions
+#define INICIO_CODIGO_ACSII_MIN 96
+#define FINAL_CODIGO_ACSII_MIN 123
+
+#define INICIO_CODIGO_ACSII_MAY 64
+#define FINAL_CODIGO_ACSII_MAY 91
+
+#define ESPACIO_CODIGO_ACSII 32
